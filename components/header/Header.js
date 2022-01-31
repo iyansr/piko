b4ab4e9995816e5017e5a296d2bfd82540f68b18
I Putu Saputrayana <ikhyan7@gmail.com>
2022-01-31 19:33:06 +0800

    Init Tailwind

---
+++ components/header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+	return (
+		<div className='h-14 bg-red-500 w-full px-6'>
+			<div className='h-full w-full max-w-6xl mx-auto bg-blue-900'></div>
+		</div>
+	)
+}
+
+export default Header
